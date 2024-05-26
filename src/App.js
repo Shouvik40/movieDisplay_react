e9546f1b8eecd2137d44f0416840fe7bf21e8380
Shouvik40 <105535909+Shouvik40@users.noreply.github.com>
2024-05-26 18:49:04 +0530

    Added all required functionalities.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MovieProvider } from "./Context/MovieContext";
+import AddMovieForm from "./Form/AddMovieForm";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import "./App.css";
+import MovieDetails from "./Pages/MovieDetils/MovieDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MovieProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route exact path="/movie/:id" element={<MovieDetails />} />
+            <Route path="/add-movie" element={<AddMovieForm />} />
+          </Routes>
+        </div>
+      </Router>
+    </MovieProvider>
   );
 }
 
